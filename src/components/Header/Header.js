cc97f406b892fd9108f8dbea13fc20dda9e9cfa7
JMAN <lambejohn6@gmail.com>
2022-08-12 03:48:33 +0100

    second commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from "react"
 import "./Header.css"
+import gpt from "./gpttoons1.png"
 
 const Header = () => {
   return (
-    <span onClick={()=> window.scroll(0, 0)} className='header'>🎥 JMAN HUB 🎥 </span>
+    <span onClick={()=> window.scroll(0, 0)} className='header'>
+      <img src={gpt} alt="gpttoons"/>
+      JMAN HUB 🎥 </span>
 
   )
 }
